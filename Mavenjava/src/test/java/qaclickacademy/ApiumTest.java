209ded9ecf02c9639b105165d0fe3a11a2463028
mrbabu1 <mandadirb@gmail.com>
2020-10-02 01:32:43 +0530

    first commit

---
+++ Mavenjava/src/test/java/qaclickacademy/ApiumTest.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class ApiumTest {
+	
+	@Test	
+	public void Test()
+	{System.out.println("Testing Apium");}
+
+}
---
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,11 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test	
+	public void Test1()
+	{System.out.println("Testing Selenium");}
+
+}
